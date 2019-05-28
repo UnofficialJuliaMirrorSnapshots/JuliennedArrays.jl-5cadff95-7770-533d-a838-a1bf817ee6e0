b982641d3186b0917c7147f33595916a27fb3439
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-05-28 18:08:55 +0000

    Snapshot of branch master taken on 2019-05-28T14:08:53.973-04:00 from "https://github.com/UnofficialJuliaMirror/JuliennedArrays.jl-5cadff95-7770-533d-a838-a1bf817ee6e0"

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,13 @@
 using JuliennedArrays
 using Documenter: makedocs, deploydocs
 
-makedocs(sitename = "JuliennedArrays.jl", strict = true)
+makedocs(
+    sitename = "JuliennedArrays.jl",
+    strict = true,
+    modules = [JuliennedArrays]
+)
 
-if get(ENV, "TRAVIS_OS_NAME", nothing) == "linux" && get(ENV, "TRAVIS_JULIA_VERSION", nothing) == "1.1"
+if get(ENV, "TRAVIS_OS_NAME", nothing) == "linux" &&
+    get(ENV, "TRAVIS_JULIA_VERSION", nothing) == "1.1"
     deploydocs(repo = "github.com/bramtayl/JuliennedArrays.jl.git")
 end
-
-using Test: @test
-
-A = rand(5, 5, 5)
-S = Slices(A, True(), False(), False())
-@test size(first(S)) == (5,)
